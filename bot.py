3b06315e702fafd386739ee78ba943662fc6b613
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-03-29 17:38:28 -0300

    Implement message handler for the regex

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
 from testfall import named
-import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import re
 
 with open('token.txt', 'r') as f:
     TOKEN=f.read()
 
+def named(update, context):
+    print('Callback called')
+    print(context.matches)
+
 def main():
-    bot = telegram.Bot(token=TOKEN)
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    regex = re.compile('\[\[([\w | \. | \s |,])*\]\]')
+    named_handler = MessageHandler(Filters.regex(regex), 'named')
+    dispatcher.add_handler(named_handler)
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
